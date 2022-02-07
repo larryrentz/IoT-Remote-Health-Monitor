6fc8705e3d46390150844c9a5f3c9357d4093d70
anngedeus <76603380+anngedeus@users.noreply.github.com>
2022-02-07 16:42:59 -0500

    widget 1

--- HealthMonitoringSystem/Frontend/frontend/src/components/Patient.js
+++ HealthMonitoringSystem/Frontend/frontend/src/components/Patient.js
@@ -1,11 +1,28 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 
 function Patient() {
     return (
-      <div>
-         <h1>COOKIES</h1>
-      </div>
-    );
-  }
-  export default Patient;
+      <Card sx={{ width: 250 }} color="blue">
+      <CardContent>
+        <Typography sx={{ fontSize: 20 }} color="black" gutterBottom>
+          Patient Information
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="black">
+            Name: Jane Doe
+        </Typography>
+        <Typography variant="body2" color="black">
+            DOB: 10/20/1975
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+export default Patient;
   
