61c0b4f1854b12b3cd71202eba9ec0676f715780
Nabil <nkoney14@outlook.com>
2022-03-17 20:44:48 -0400

    Reverted last commit for consistency

--- HealthMonitoringSystem/Frontend/frontend/src/components/Firebase.js
+++ HealthMonitoringSystem/Frontend/frontend/src/components/Firebase.js
@@ -1,20 +1,20 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import firebase from 'firebase/compat/app'
+
+import "firebase/compat/firestore"
 import "firebase/compat/auth"
 import "firebase/compat/functions"
 
-const firebaseConfig = {
+firebase.initializeApp({
     apiKey: "AIzaSyBFuLFk95pOi0FkcsMAdaq8OSuGWffpYic",
     authDomain: "iot-remote-health-monitor.firebaseapp.com",
     projectId: "iot-remote-health-monitor",
     storageBucket: "iot-remote-health-monitor.appspot.com",
     messagingSenderId: "72011895649",
     appId: "1:72011895649:web:7582011578b56eba766b20"
-};
+});
 
-const app = firebase.initializeApp(firebaseConfig);
-export const firestore = getFirestore(app);
 export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 export const functions = firebase.functions();
 
-export default app;
+export default firebase;
