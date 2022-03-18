813d4f882c61f60f80c254c7bbface04847f419f
Nabil <nkoney14@outlook.com>
2022-03-17 20:32:35 -0400

    Refactored code according to the new documentation: https://firebase.google.com/docs/web/modular-upgrade

--- HealthMonitoringSystem/Frontend/frontend/src/components/Firebase.js
+++ HealthMonitoringSystem/Frontend/frontend/src/components/Firebase.js
@@ -1,20 +1,20 @@
-import firebase from 'firebase/compat/app'
-
-import "firebase/compat/firestore"
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import "firebase/compat/auth"
 import "firebase/compat/functions"
 
-firebase.initializeApp({
+const firebaseConfig = {
     apiKey: "AIzaSyBFuLFk95pOi0FkcsMAdaq8OSuGWffpYic",
     authDomain: "iot-remote-health-monitor.firebaseapp.com",
     projectId: "iot-remote-health-monitor",
     storageBucket: "iot-remote-health-monitor.appspot.com",
     messagingSenderId: "72011895649",
     appId: "1:72011895649:web:7582011578b56eba766b20"
-});
+};
 
+const app = firebase.initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
 export const auth = firebase.auth();
-export const firestore = firebase.firestore();
 export const functions = firebase.functions();
 
-export default firebase;
+export default app;
