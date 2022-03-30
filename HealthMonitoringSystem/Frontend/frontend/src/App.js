b97a9a7f02dccd0d9236df05e91f432265ac0e8f
Nabil <nkoney14@outlook.com>
2022-03-30 02:08:38 -0400

    added grid layout to main container and added FAQs button for modal

--- HealthMonitoringSystem/Frontend/frontend/src/App.js
+++ HealthMonitoringSystem/Frontend/frontend/src/App.js
@@ -1,11 +1,45 @@
 import './App.css';
+import { Box, Typography } from '@mui/material';
 import Patient from './components/Patient.js'
+import FAQs from './components/FAQs.js'
 
 function App() {
   return (
-    <div>
-      <Patient/>  
-    </div>
+    <Box
+      sx={{
+        width: '100%',
+        height: '140px',
+        '& > .MuiBox-root > .MuiBox-root': {
+          p: 1,
+          borderRadius: 2,
+          margin: 1,
+          fontWeight: '700'
+        },
+      }}
+    >
+      <Box
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(4, 1fr)',
+          gridTemplateAreas: `"header header header header" "main main main sidebar" "footer footer footer sidebar"`
+        }}
+      >
+        <Box sx={{ gridArea: 'header'}}>
+          <Typography> IoT Remote Health Monitor </Typography>
+        </Box>
+        <Box sx={{ gridArea: 'main' }}>
+          <Patient /> 
+        </Box>
+        <Box sx={{ gridArea: 'sidebar'}}>
+          <Typography> Notifications </Typography>
+          <Typography> Sidebar </Typography>
+          <FAQs/> 
+        </Box>
+        <Box sx={{ gridArea: 'footer'}}>
+          <Typography> Made with Love :) </Typography>
+        </Box>
+      </Box>
+    </Box>
   );
 }
 export default App;
