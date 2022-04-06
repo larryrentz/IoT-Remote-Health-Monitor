ba8324f3141d9f8d3cd38f5e0f96ab587bc755ed
Nabil <nkoney14@outlook.com>
2022-04-05 21:38:49 -0400

    added app level context

--- HealthMonitoringSystem/Frontend/frontend/src/App.js
+++ HealthMonitoringSystem/Frontend/frontend/src/App.js
@@ -1,72 +1,33 @@
 import './App.css';
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useContext, useState} from 'react';
-import { Box, Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react';
 import Signin from './components/SignIn';
-import Patient from './components/Patient.js'
-import FAQs from './components/FAQs.js'
-import Alerts from './components/Alerts'
-import ProfilePicture from './components/ProfilePicture';
+import Dashboard from './components/Dashboard';
 import { auth } from './components/Firebase';
-import React from 'react';
+import Context from './Context';
 
 function App() {
-  //Get current user
+  const [supportsBluetooth, setSupportsBluetooth] = useState(false);
+
+  //Get current user and store use as the default for app context
   const [user] = useAuthState(auth);
+  const [context, setContext] = useState({user, devices: []});
+
+  // When the component mounts, check that the browser supports Bluetooth
+  useEffect(() => {
+    if (navigator.bluetooth) {
+      setSupportsBluetooth(true);
+    }
+  }, []);
 
   const signInPage = <Signin />;
-
-  const app = (
-    <Box
-      sx={{
-        width: '100%',
-        height: '140px',
-        '& > .MuiBox-root > .MuiBox-root': {
-          p: 1,
-          borderRadius: 2,
-          margin: 1,
-          fontWeight: '700'
-        },
-      }}
-    >
-      <Box
-        sx={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(4, 1fr)',
-          gridTemplateAreas: `"header header header header" "main main main sidebar" "footer footer footer sidebar"`
-        }}
-      >
-        <Box sx={{ gridArea: 'header', bgcolor: 'lightgreen'}}>
-          <Typography variant="h4" sx={{ textAlign: 'center'}}> IoT Remote Health Monitor </Typography>
-        </Box>
-        <Box sx={{ gridArea: 'main', bgcolor: 'lightyellow' }}>
-          <Patient /> 
-        </Box>
-        <Box sx={{ gridArea: 'sidebar', bgcolor: 'lightblue'}}>
-          <Box sx={{display: 'flex', justifyContent: 'center'}}>
-            <ProfilePicture />
-          </Box>
-          <Typography> Notifications </Typography>
-          <Box sx={{gridArea: 'sidebar', justifySelf:'right', allignSelf:'end'}}>
-                <Alerts/>
-          </Box>
-        </Box>
-        <Box sx={{ gridArea: 'footer', bgcolor: 'lightgray'}}>
-          <Typography sx={{ textAlign: 'center'}}>
-            Made with Love :)
-          </Typography>
-        </Box>
-        <Box sx={{ gridArea: 'sidebar', justifySelf: 'end', alignSelf: 'end'}}>
-          <FAQs/> 
-        </Box>
-      </Box>
-    </Box>
-  );
+  const dashboard = <Dashboard />;
 
   return (
-    <div>
-      {user ? app : signInPage} 
-    </div>
+    <Context.Provider value={{context, setContext}}>
+      {user && supportsBluetooth && dashboard}
+      {!user && supportsBluetooth && signInPage} 
+    </Context.Provider>
   );
 }
 export default App;
