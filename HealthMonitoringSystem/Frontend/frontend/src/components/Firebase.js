4a7a0ee61752e788714fa13018453f32b44c66e4
Nabil <nkoney14@outlook.com>
2022-04-06 23:10:21 -0400

    changed config for firebase

--- HealthMonitoringSystem/Frontend/frontend/src/components/Firebase.js
+++ HealthMonitoringSystem/Frontend/frontend/src/components/Firebase.js
@@ -4,22 +4,13 @@ import "firebase/compat/firestore"
 import "firebase/compat/auth"
 import "firebase/compat/functions"
 
-// firebase.initializeApp({
-//     apiKey: "AIzaSyBFuLFk95pOi0FkcsMAdaq8OSuGWffpYic",
-//     authDomain: "iot-remote-health-monitor.firebaseapp.com",
-//     projectId: "iot-remote-health-monitor",
-//     storageBucket: "iot-remote-health-monitor.appspot.com",
-//     messagingSenderId: "72011895649",
-//     appId: "1:72011895649:web:7582011578b56eba766b20"
-// });
-
 firebase.initializeApp({
-    apiKey: "AIzaSyDGC2SCuqZHB7rBblDPyTNJflmuMmULD10",
-    authDomain: "remote-health-monitor-740d6.firebaseapp.com",
-    projectId: "remote-health-monitor-740d6",
-    storageBucket: "remote-health-monitor-740d6.appspot.com",
-    messagingSenderId: "1031641000421",
-    appId: "1:1031641000421:web:d9c94e16b830ae3096e70c"
+  apiKey: "AIzaSyB1hHKIUaKTbHS38-_OQLT7ROcev2UcTps",
+  authDomain: "remote-health-monitor-9c217.firebaseapp.com",
+  projectId: "remote-health-monitor-9c217",
+  storageBucket: "remote-health-monitor-9c217.appspot.com",
+  messagingSenderId: "747805954696",
+  appId: "1:747805954696:web:5bf85297b59b83baf9c53d"
 });
 
 export const auth = firebase.auth();
